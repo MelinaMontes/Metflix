94bf30130925141866af378daf4082df4d4e7cdc
MeLiNa <78213064+MelinaMontes@users.noreply.github.com>
2021-04-03 10:36:12 -0300

    Update Episodio.java

--- Episodio.java
+++ Episodio.java
@@ -1,10 +1,9 @@
 public class Episodio {
     public int numero;
     public String nombre;
-    public int duracion; //en minutos
+    public int duracion; 
     public Director director;
     public String sinopsis;
-
     public void reproducir(){
         System.out.println("Reproduciendo episodio " + nombre);
     }

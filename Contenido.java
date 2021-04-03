9d05ecdf89450c836e74d4889392d01238a503d5
MeLiNa <78213064+MelinaMontes@users.noreply.github.com>
2021-04-03 10:35:14 -0300

    Update Contenido.java

--- Contenido.java
+++ Contenido.java
@@ -4,7 +4,6 @@ import java.util.List;
 public class Contenido {
     public String nombre;
     public Genero genero;
-    //Idioma Origne
     public Idioma idiomaOrigen;
     public int añoLanzamiento;
     public List<Actor> actores = new ArrayList<>();
@@ -13,8 +12,7 @@ public class Contenido {
 
     public Director buscarDirector(String nombre){
         for (Director director: this.directores){
-            if(director.nombre.equals(nombre)){ //si esto es verdadero, encontre la temporada
-                return director;
+            if(director.nombre.equals(nombre)){ 
             }
             
         }

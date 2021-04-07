d2f003edd94b185032fb13328eb99b0de2493c0f
MelinaMontes <you@example.com>
2021-04-07 18:40:21 -0300

    Se añadio la clase Websodio, para visualizar episodios desde una pagina web.

--- src/App.java
+++ src/App.java
@@ -1,30 +1,50 @@
+import java.util.*;
+
 public class App {
+
+    public static Scanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        //A partir de Metflix, buscar la temporada 5 episodio 1 de la serie
-        //How I met your mother y 
-        //reproducir el episodio.
+     
 
         Metflix miMetflix = new Metflix();
 
         System.out.println("Inicializando catalogo de Metflix");
 
-        miMetflix.inicializarCatalogo();//Estoy llamando al metodo que crea las pelis y series.
+        miMetflix.inicializarCatalogo();.
 
-        Serie serieBuscada = miMetflix.buscarSerie("Dark");
+        Serie serieBuscada = miMetflix.buscarSerie("How I met your mother");
 
-        Temporada temporada = serieBuscada.buscarTemporada(2);
+        if (serieBuscada == null) {
+            System.out.println("No se encontro la serie");
+            return;
+        }
+        System.out.println("Ingrese nro temporada: ");
 
-        Episodio episodio = temporada.buscarEpisodio(4);
+        int nroTemporada = Teclado.nextInt();
+        Teclado.nextLine();
+
+        Temporada temporada = serieBuscada.buscarTemporada(nroTemporada);
+
+        System.out.println("Ingrese nro episodio: ");
+        int nroEpisodio = Teclado.nextInt();
+        Teclado.nextLine();
+
+        Episodio episodio = temporada.buscarEpisodio(nroEpisodio);
 
         episodio.reproducir();
-
-        Contenido miContenido = new Contenido();
-  
-        Director director = miContenido.buscarDirector(null);
-
-        System.out.println("El director buscado es: " + director);
+        
+        if(episodio instanceof Websodio){
+            System.out.println("El episodio era un websodio");
+        }
+        else {
+            System.out.println("Es un episodio");
+        }
+        if (episodio instanceof Websodio){
+            Websodio websodio = (Websodio)episodio;
+            System.out.println("El link del websodio es: "+ websodio.link);
+        }
 
 
     }
 }
-

bfda04666a3b7bec5d6ae6c26ffa21d66e6f0761
MeLiNa <78213064+MelinaMontes@users.noreply.github.com>
2021-04-07 18:57:42 -0300

    Add files via upload
    Se añade la clase Websodio, para acceder al contenido desde un link.

---
+++ Websodio.java
@@ -0,0 +1,11 @@
+public class Websodio extends Episodio {
+    public String link;
+
+    @Override
+    public void reproducir() {
+        System.out.println("Reproduciendo websodio del link " + this.link);
+    }
+}
+
+
+
